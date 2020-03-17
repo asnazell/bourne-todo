585c7b5833406816dc259af7861f6014be721a17
Assel Snazell <assel.snazell@gmail.com>
2020-03-17 17:53:23 +1100

    add form validation

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,20 +1,41 @@
 import React from "react";
 import { Button, TextField } from "@material-ui/core";
+import { useForm } from "react-hook-form";
 
-export const TodoAdd = () => (
-  <section className="todo-add">
-    <form className="form-task-add" noValidate autoComplete="off">
-      <div className="textfield">
-        <TextField
-          label="New Task"
-          placeholder="What needs to get done?"
-          variant="outlined"
-          fullWidth
-        />
-      </div>
-      <Button variant="outlined" color="primary">
-        Add
-      </Button>
-    </form>
-  </section>
-);
+export const TodoAdd = () => {
+  const { handleSubmit, register, errors } = useForm();
+
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  return (
+    <section className="todo-add">
+      <form
+        className="form-task-add"
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="textfield">
+          <TextField
+            name="task"
+            label="New Task"
+            placeholder="What needs to get done?"
+            variant="outlined"
+            autoFocus
+            fullWidth
+            error={!!errors.task}
+            helperText={errors.task && errors.task.message}
+            inputRef={register({
+              required: "Please enter a description of your task"
+            })}
+          />
+        </div>
+        <Button type="submit" variant="outlined" color="primary">
+          Add
+        </Button>
+      </form>
+    </section>
+  );
+};
